3615af5d534cf26049470ed88dbeafe6d626f9e7
sarcos <soxos.inc@gmail.com>
2011-02-16 23:52:51 -0500

    app client

--- lib/mogli/app_client.rb
+++ lib/mogli/app_client.rb
@@ -3,10 +3,20 @@ require "mogli/client/user"
 
 module Mogli
   class AppClient < Client
-    attr_accessor :application_id
+    attr_accessor :application_id, :user_id, :message, :data, :app_at
     
     def subscription_url
       "https://graph.facebook.com/#{application_id}/subscriptions"
+    end
+    
+    def app_request_url
+      "https://graph.facebook.com/#{user_id}/apprequests?message='#{message}'&data='#{data}'&app_access_token=#{app_at}&method=post"
+    end
+    
+    def app_request
+      options_to_send = options.dup
+      options_to_send[:fields] = Array(options[:fields]).join(",")
+      self.class.post(app_request_url,:body=>default_params.merge(options_to_send))
     end
     
     def subscribe_to_model(model,options)
@@ -23,5 +33,7 @@ module Mogli
     def subscriptions
       get_and_map_url(subscription_url,"Subscription")
     end
+    
+    
   end
 end
